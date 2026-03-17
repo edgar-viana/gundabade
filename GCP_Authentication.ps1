81419de58bcf4a9d9580b983c55b90fa88f9f4c9
edgar-viana <117957680+edgar-viana@users.noreply.github.com>
2026-03-17 12:16:20 -0300

    fluxo_wallpaper_lockscreen

---
+++ GCP_Authentication.ps1
@@ -0,0 +1,12 @@
+﻿#Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser -Force
+
+$project = "shared-architecture-471814"
+
+gcloud auth activate-service-account `
+"workplace-sync-prd-sa@shared-architecture-471814.iam.gserviceaccount.com" `
+--key-file="C:\scripts\fluxo_wallpaper_lockscreen\key_bucket_eng_workplace.json"
+
+#gcloud auth login
+#gcloud config set project $project
+gsutil cp C:\Wallpaper\JV-BR-Default-Lockscreen.jpg gs://engenharia-workplace/
+gsutil cp C:\Wallpaper\JV-BR-Default-Wallpaper.jpg gs://engenharia-workplace/
